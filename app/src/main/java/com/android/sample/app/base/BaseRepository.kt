2b03543d3fae23945970ebbbd28b0fb3b1ebc19a
Akshata Guntapelliwar <Akshata.Guntapelliwar@freecharge.com>
2023-12-11 11:09:57 +0530

    Initial commit

---
+++ app/src/main/java/com/android/sample/app/base/BaseRepository.kt
@@ -0,0 +1,50 @@
+package com.android.sample.app.base
+
+import android.content.Context
+import com.android.sample.app.R
+import com.android.sample.app.util.ViewState
+import com.android.sample.app.util.isNetworkAvailable
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import timber.log.Timber
+
+abstract class BaseRepository<T>(
+    private val context: Context,
+    private val ioDispatcher: CoroutineDispatcher
+) {
+    protected abstract suspend fun query(id: String?): T?
+
+    protected abstract suspend fun fetch(url: String?): T
+
+    protected abstract suspend fun saveFetchResult(t: T)
+
+    fun getResult(id: String?, url: String?): Flow<ViewState<T?>> = flow {
+        emit(ViewState.Loading)
+        query(id)?.let {
+            emit(ViewState.Success(it))
+            try {
+                refresh(url)
+                emit(ViewState.Success(query(id)))
+            } catch (t: Throwable) {
+                Timber.e(t)
+            }
+        } ?: run {
+            if (context.isNetworkAvailable()) {
+                try {
+                    refresh(url)
+                    emit(ViewState.Success(query(id)))
+                } catch (t: Throwable) {
+                    emit(ViewState.Error(context.getString(R.string.failed_loading_msg)))
+                }
+            } else {
+                emit(ViewState.Error(context.getString(R.string.failed_network_msg)))
+            }
+        }
+    }.flowOn(ioDispatcher)
+
+    private suspend fun refresh(url: String?) {
+        saveFetchResult(fetch(url))
+    }
+}
